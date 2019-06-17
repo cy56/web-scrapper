97332b31c479734b7f1517828e73872feeefc393
cy56 <limchunyao@gmail.com>
2019-06-17 20:42:06 +0800

    missing worker.js

--- worker.js
+++ worker.js
@@ -1,2 +1,28 @@
-const worker = require('./app/services/worker');
-worker(['gen'], {start:'2019-05-01', end:'2019-05-01'});
+class Worker {
+    constructor(params = { vendor: null, start: null, end: null, brand: null }) {
+        if (params.vendor == null || params.start == null || params.brand == null) {
+            throw "missing parameters";
+        }
+
+        this.vendor = params.vendor.toUpperCase();
+        this.start = params.start;
+        this.end = params.end || params.start;
+        this.brand = params.brand.toUpperCase();
+        this.module = require(`./app/services/puppeteer/jobs/${params.vendor}`);
+
+        return this;
+    }
+
+    async work() {
+        await this.module(this.start, this.end, this.brand);
+    }
+}
+
+async function work(jobs = [], options = { start: null, end: null, brand:null }) {
+    jobs.forEach(async (job) => {
+        const worker = new Worker({ vendor: job, start: options.start, end: options.end, brand: options.brand });
+        await worker.work();
+    });
+}
+
+work(['pt'], {start:'2019-05-01', end:'2019-05-01', brand:'rb88'});
